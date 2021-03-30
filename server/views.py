b25644a43750bcab698c25cd4c66187ee948121b
Saksham Malhotra <saksham2196@gmail.com>
2021-03-31 00:15:28 +0530

    add NodeHandler

--- server/views.py
+++ server/views.py
@@ -12,21 +12,29 @@ class CaduceusHandler(tornado.web.RequestHandler):
 class DagInfo(tornado.web.RequestHandler):
     def get(self):
         dag_props = {
-            "nodes": self.application.dag.nodes,
-            "edges": self.application.dag.edges,
+            "nodes": [str(_) for _ in self.application.dag.nodes],
+            "edges": [str(_) for _ in self.application.dag.edges],
         }
         self.write(dag_props)
 
 
-class AddNode(CaduceusHandler):
-    def get(self):
+class NodeHandler(CaduceusHandler):
+    def get(self, node_id=None):
+        if node_id:
+            node = self.application.dag.get_node(node_id)
+            print(node.__dict__)
+            node_props = vars(node)
+            return self.write({"response": [node_props]})
+        nodes = self.application.dag.nodes
+        node_props = [vars(_) for _ in nodes]
+        return self.write({"response": node_props})
 
-        node = MercuriNode(
-            input={"ai_1": "ai_1", "ai_2": "ai_2"},
-            output={},
-            docker_volume="/usr/src/app",
-            docker_img_name="nodea",
-        )
+    # _ parameter added as post expects two arguments from route
+    def post(self, _):
+        data = json.loads(self.request.body)
+        node = MercuriNode(**data)
+
         self.application.dag.add_node(node)
         print(self.application.dag.nodes)
+        print("node added: ", node.id)
         self.write("Nodes: " + str(len(self.application.dag.nodes)))
