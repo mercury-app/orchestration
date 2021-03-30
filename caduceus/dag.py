d7ed6dd2b60ff4bfabcfad641bc37547e6adaba8
Saksham Malhotra <saksham2196@gmail.com>
2021-03-31 00:14:34 +0530

    add get_node and nodes property to dag

--- caduceus/dag.py
+++ caduceus/dag.py
@@ -1,28 +1,35 @@
 import networkx as nx
+from typing import List
 
 from caduceus.node import MercuriNode
 
 
 class MercuriDag:
     def __init__(self):
-        self.nx_dag = nx.DiGraph()
+        self._nxdag = nx.DiGraph()
 
     @property
-    def nodes(self) -> list:
-        return [str(_node) for _node in self.nx_dag.nodes]
+    def nodes(self) -> List[MercuriNode]:
+        return list(self._nxdag.nodes)
 
     @property
     def edges(self) -> list:
-        return [(str(_[0]), str(_[1])) for _ in self.nx_dag.edges]
+        return [(str(_[0]), str(_[1])) for _ in self._nxdag.edges]
 
     def add_node(self, node: MercuriNode) -> None:
-        self.nx_dag.add_node(node)
+        self._nxdag.add_node(node, id=node.id)
 
     def remove_node(self, node: MercuriNode) -> None:
-        self.nx_dag.remove_node(node)
+        self._nxdag.remove_node(node)
 
     def add_edge(self, node1: MercuriNode, node2: MercuriNode) -> None:
-        self.nx_dag.add_edge(node1, node2)
+        self._nxdag.add_edge(node1, node2)
 
-    # nx_dag.successors(node1)
-    # nx_dag.predescessors(node1)
+    def get_node(self, id: str) -> MercuriNode:
+        node_search = [_ for _ in self._nxdag.nodes if _.id == id]
+        assert len(node_search) < 2
+        if len(node_search) == 1:
+            return node_search[0]
+
+    # _nxdag.successors(node1)
+    # _nxdag.predescessors(node1)
