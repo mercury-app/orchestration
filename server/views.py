2a62b41e237795bf9871e76a0ce979cd0fd6d964
Saksham Malhotra <saksham2196@gmail.com>
2021-04-06 01:47:58 +0530

    add container id in post return

--- server/views.py
+++ server/views.py
@@ -9,7 +9,7 @@ class CaduceusHandler(tornado.web.RequestHandler):
         self.write("Welcome to Caduceus")
 
 
-class DagInfo(tornado.web.RequestHandler):
+class DagInfo(CaduceusHandler):
     def get(self):
         dag_props = {
             "nodes": [str(_) for _ in self.application.dag.nodes],
@@ -22,26 +22,33 @@ class NodeHandler(CaduceusHandler):
     def get(self, node_id=None):
         if node_id:
             node = self.application.dag.get_node(node_id)
-            node_props = vars(node)
-            return self.write({"response": [node_props]})
+            nodes = [node]
+        else:
+            nodes = self.application.dag.nodes
 
-        nodes = self.application.dag.nodes
-        node_props = [vars(_) for _ in nodes]
+        node_props = [
+            {
+                "id": _.id,
+                "container": {
+                    "container_id": _.caduceus_container.container_id,
+                    "container_state": _.caduceus_container.container_state,
+                },
+            }
+            for _ in nodes
+        ]
         return self.write({"response": node_props})
 
     # _ parameter added as post expects two arguments from route
     def post(self, _):
         data = json.loads(self.request.body)
         node = MercuriNode(**data)
+        node.initialise_container()
         self.application.dag.add_node(node)
 
         response = {
             "response": {
-                "dag_info": {
-                    "nodes": len(self.application.dag.nodes),
-                    "edges": len(self.application.dag.edges),
-                },
                 "id": node.id,
+                "container_id": node.caduceus_container.container_id,
             }
         }
         self.write(response)
@@ -63,5 +70,28 @@ class NodeHandler(CaduceusHandler):
         node = self.application.dag.get_node(node_id)
         self.application.dag.remove_node(node)
 
-        node_props = vars(node)
+        node_props = {
+            "id": node.id,
+            "container": {
+                "container_id": node.caduceus_container.container_id,
+                "container_state": node.caduceus_container.container_state,
+            },
+        }
         self.write({"response": [node_props]})
+
+
+class TriggerNode(CaduceusHandler):
+    def put(self, node_id):
+        print("triggered put")
+        data = json.loads(self.request.body)
+        node = self.application.dag.get_node(node_id)
+
+        trigger = data.get("status", None)
+
+        if trigger == "run":
+            print("triggering node")
+            container_id = await node.trigger()
+
+        response = {"response": {"container": {"id": container_id}}}
+
+        return response
