89f5fdf790b4dc2cb634e4b64c5cb7c20ab2be73
Saksham Malhotra <saksham2196@gmail.com>
2021-04-09 02:33:52 +0530

    update edge setter

--- caduceus/edge.py
+++ caduceus/edge.py
@@ -25,19 +25,31 @@ class MercuriEdge:
     def source_node(self) -> MercuriNode:
         return self._source_node
 
+    @source_node.setter
+    def source_node(self, node: MercuriNode):
+        self._source_node = node
+
     @property
     def dest_node(self) -> MercuriNode:
         return self._dest_node
 
+    @dest_node.setter
+    def dest_node(self, node: MercuriNode):
+        self._dest_node = node
+
     @property
-    def source_dest_connect(self) -> list:
+    def source_dest_connect(self) -> set:
         return self._source_dest_connect
 
-    def add_source_dest_connect(self, source_dest_pair: Tuple[str, str]):
-        if source_dest_pair[0] not in self._source_node.output.keys():
-            raise Exception("source output is not an output of the source node")
+    @source_dest_connect.setter
+    def source_dest_connect(self, source_dest_map: set):
+        for source_dest_pair in source_dest_map:
+            assert len(source_dest_pair) == 2
+            if source_dest_pair[0] not in self._source_node.output.keys():
+                raise Exception("source output is not an output of the source node")
+            if source_dest_pair[1] not in self._dest_node.input.keys():
+                raise Exception(
+                    "destination input is not an input of the destination node"
+                )
 
-        if source_dest_pair[1] not in self._dest_node.input.keys():
-            raise Exception("destination input is not an input of the destination node")
-
-        self._source_dest_connect.add(source_dest_pair)
+        self._source_dest_connect = source_dest_map
