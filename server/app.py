a3b497a32d625a6efd2133d70f318961e9c9dda0
Saksham Malhotra <saksham2196@gmail.com>
2021-04-09 01:37:25 +0530

    import new views

--- server/app.py
+++ server/app.py
@@ -1,10 +1,21 @@
 import tornado.web  # for handlers
 import tornado.ioloop  # for listening to port
+import logging
 
 from caduceus.dag import MercuriDag
 from caduceus.node import MercuriNode
 
-from server.views import CaduceusHandler, NodeHandler, DagInfo
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+from server.views import (
+    CaduceusHandler,
+    NodeHandler,
+    NodeContainerHandler,
+    DagInfo,
+    EdgeHandler,
+)
 
 
 class Application(tornado.web.Application):
@@ -14,7 +25,9 @@ class Application(tornado.web.Application):
         self.dag = MercuriDag()
         handlers = [
             (r"/", CaduceusHandler),
+            (r"/nodes/([^/]+)?/trigger", NodeContainerHandler),
             (r"/nodes/([^/]+)?", NodeHandler),
+            (r"/edges/([^/]+)?", EdgeHandler),
             (r"/dag", DagInfo),
         ]
         super().__init__(handlers, debug=True)
@@ -23,4 +36,5 @@ class Application(tornado.web.Application):
 if __name__ == "__main__":
     app = Application()
     app.listen(8888)
+    logging.info("running on port 8888")
     tornado.ioloop.IOLoop.current().start()
