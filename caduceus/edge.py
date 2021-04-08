b9f850e9b144b909c2865677e31dc8065b3365c1
Saksham Malhotra <saksham2196@gmail.com>
2021-04-09 01:36:46 +0530

    add edge class

---
+++ caduceus/edge.py
@@ -0,0 +1,43 @@
+import logging
+from typing import Tuple
+from uuid import uuid4
+
+from caduceus.node import MercuriNode
+
+
+class MercuriEdge:
+    def __init__(
+        self,
+        source_node: MercuriNode = None,
+        dest_node: MercuriNode = None,
+        source_dest_connect: set = {},
+    ):
+
+        self.id = uuid4().hex
+        self._source_node = source_node
+        self._dest_node = dest_node
+
+        # the connections between source output set and destination input set have to
+        # be one-one (injective) but not necessarily onto(surjective)
+        self._source_dest_connect = source_dest_connect
+
+    @property
+    def source_node(self) -> MercuriNode:
+        return self._source_node
+
+    @property
+    def dest_node(self) -> MercuriNode:
+        return self._dest_node
+
+    @property
+    def source_dest_connect(self) -> list:
+        return self._source_dest_connect
+
+    def add_source_dest_connect(self, source_dest_pair: Tuple[str, str]):
+        if source_dest_pair[0] not in self._source_node.output.keys():
+            raise Exception("source output is not an output of the source node")
+
+        if source_dest_pair[1] not in self._dest_node.input.keys():
+            raise Exception("destination input is not an input of the destination node")
+
+        self._source_dest_connect.add(source_dest_pair)
