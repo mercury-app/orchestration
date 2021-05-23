ef265a130d4b17ee8a6f8b55d4eee023aac06bf3
Saksham Malhotra <saksham2196@gmail.com>
2021-05-23 17:29:35 +0530

    add assertions for json api

--- server/views/__init__.py
+++ server/views/__init__.py
@@ -1,9 +1,29 @@
 import tornado.web
 import logging
+from typing import List
+import json
 
 logger = logging.getLogger(__name__)
 
 
 class CaduceusHandler(tornado.web.RequestHandler):
+    def prepare(self):
+        if self.request.method in ["POST", "PATCH"]:
+            assert (
+                self.request.headers.get("Content-Type") == "application/vnd.api+json"
+            )
+            assert self.request.headers.get("Accept") == "application/vnd.api+json"
+
+            data = json.loads(self.request.body)
+            assert "data" in data.keys()
+            assert isinstance(data["data"], dict)
+            assert "type" in data["data"]
+
+            if self.request == "PATCH":
+                assert "id" in data.keys()
+
+        elif self.request.method in ["GET"]:
+            assert self.request.headers.get("Accept") == "application/vnd.api+json"
+
     def get(self):
         self.write("Welcome to Caduceus")
