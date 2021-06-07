57e08c7054eecd5862f5f87fc4f116bea767b8b3
Saksham Malhotra <saksham2196@gmail.com>
2021-06-07 20:26:54 +0530

    code to read json for an edge

--- mercury/edge.py
+++ mercury/edge.py
@@ -1,8 +1,10 @@
 import logging
 from typing import Tuple
 from uuid import uuid4
+import os
 
 from mercury.node import MercuryNode
+from mercury.constants import BASE_DOCKER_BIND_VOLUME
 
 
 class MercuryEdge:
@@ -16,6 +18,12 @@ class MercuryEdge:
         self.id = uuid4().hex
         self._source_node = source_node
         self._dest_node = dest_node
+        self._json_path = f"{BASE_DOCKER_BIND_VOLUME}/{self.id}.json"
+
+        with open(self._json_path) as f:
+            io = json.load(f)
+
+        self._json_inputs = io.keys().tolist()
 
         # the connections between source output set and destination input set have to
         # be one-one (injective) but not necessarily onto(surjective)
@@ -42,3 +50,23 @@ class MercuryEdge:
     @property
     def source_dest_connect(self) -> list:
         return self._source_dest_connect
+
+    @property
+    def json_inputs(self) -> list:
+        return self._json_inputs
+
+    def get_input_code_snippet(self) -> str:
+        if not os.path.exists(self._json_path):
+            raise Exception(f"json for edge {self.id} doesn't exist")
+
+        with open(self._json_path) as f:
+            io = json.load(f)
+
+        code_lines = []
+
+        for k, v in io.items():
+            code_line = f"{k} = {v}"
+            code_lines.append(code_line)
+
+        code = "\n".join(code_lines)
+        return code
